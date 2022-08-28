b54c4c64eb937c79692c2cbc6a27cf3607b0802e
Dennis Karlsson <tougent@gmail.com>
2022-08-28 19:39:11 +0200

    product update

--- src/react/components/pages/product.js
+++ src/react/components/pages/product.js
@@ -3,12 +3,48 @@ import { ProductList } from '@comp/products/productList';
 
 export const Product = () => {
 
+    const boxLoop = [
+        {
+            title: 'Handsfree'
+        },
+        {
+            title: 'In-Ear',
+        },
+        {
+            title: 'On-Ear',
+        },
+        {
+            title: 'Over-Ear',
+        },
+        {
+            title: 'Brusreducering',
+        },
+        {
+            title: 'Trådlös',
+        },
+        {
+            title: 'Mikrofon',
+        }
+    ]
+
+    const box = boxLoop.map((item, index) => {
+        return (
+            <div className='product_box'>
+                <img src='./assets/images/headphones_audio_icon.svg' alt='bild på specifik hörlur'/>
+                <p className='product__title'>{item.title}</p>
+            </div>
+        )
+    }) 
+
     return (
         <div className='container'>
             
+            <h2 className='filter_title'>Filter</h2>
+
             <div className='filter'>
-               
+               {box}
             </div>
+
             <div className='navigationText'>{`Hem > Produkter > HeadPhones`}</div>
             <ProductList />
         </div>
